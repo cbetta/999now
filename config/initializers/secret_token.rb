d730435b96657df37c4a8c19eae596722d58953d
Cristiano Betta <cbetta@gmail.com>
2011-08-06 13:23:06 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helpmenow::Application.config.secret_token = '335caabd6e2e7b5821a9105e417ecb9ca2f2dc0e6f86154df11d8b47d8e172e81d84f9da64ce4f3c3c3cc6d8b3c6ad07389fdc10da5916e2f619f52b4ca3e722'
