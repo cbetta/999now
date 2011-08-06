7166c12af8b5162860b43b41ba0924033e6d2837
Cristiano Betta <cbetta@gmail.com>
2011-08-06 14:20:02 +0100

    Added phone nr validation (cbetta)

--- app/models/authorisation.rb
+++ app/models/authorisation.rb
@@ -1,3 +1,14 @@
 class Authorisation < ActiveRecord::Base
   attr_accessible :phone_number
+  
+  validates_format_of :phone_number,
+      :message => "must be a valid UK telephone number.",
+      :with => /^07[0-9]{9}$/
+      
+      
+  before_validation :convert_number
+  
+  def convert_number
+    self.phone_number = self.phone_number.gsub(/[^0-9]/, "")
+  end
 end
